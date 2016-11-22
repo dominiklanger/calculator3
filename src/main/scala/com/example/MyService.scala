bc6e29464afed09703c8af3f572dceae2a04028f
Dominik Langer <dlanger@swissonline.ch>
2016-11-22 03:53:13 +0100

    Summation and subtraction microservices implemented

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -4,6 +4,9 @@ import akka.actor.Actor
 import spray.routing._
 import spray.http._
 import MediaTypes._
+import spray.json._
+import DefaultJsonProtocol._
+
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -19,6 +22,16 @@ class MyServiceActor extends Actor with MyService {
   def receive = runRoute(myRoute)
 }
 
+case class Summation(summand1: Int, summand2: Int, sum: Int)
+case class Subtraction(minuend: Int, subtrahend: Int, difference: Int)
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val summationFormat = jsonFormat3(Summation)
+  implicit val subtractionFormat = jsonFormat3(Subtraction)
+}
+
+import MyJsonProtocol._
+import spray.json._
 
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
@@ -35,6 +48,26 @@ trait MyService extends HttpService {
             </html>
           }
         }
-      }
-    }
+      } 
+    } ~		
+	path("sum" / IntNumber) { summand1 =>
+		parameters("addend".as[Int]) { summand2 =>
+			get {
+				val summation = new Summation(summand1, summand2, summand1 + summand2)
+				respondWithMediaType(MediaTypes.`application/json`) {
+					complete(summation.toJson.prettyPrint)
+				}
+			}
+		}
+	} ~
+	path("difference" / IntNumber) { minuend =>
+		parameters("subtrahend".as[Int]) { subtrahend =>
+			get {
+				val subtraction = new Subtraction(minuend, subtrahend, minuend - subtrahend)
+				respondWithMediaType(MediaTypes.`application/json`) {
+					complete(subtraction.toJson.prettyPrint)
+				}
+			}
+		}
+	}
 }
